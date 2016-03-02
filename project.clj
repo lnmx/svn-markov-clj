0dbefb08cc17f57edb2a43e9327e8d42a11cbb8f
lnmx <len@lnmx.org>
2016-03-02 17:52:38 -0500

    MIT license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject svn-markov "0.1.0-SNAPSHOT"
   :description "generate fake commit messages from svn history"
   :url "http://github.com/lnmx/svn-markov-clj"
-  :license {:name "Eclipse Public License"
-            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT"
+            :url  "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :main ^:skip-aot svn-markov.core
   :target-path "target/%s"
